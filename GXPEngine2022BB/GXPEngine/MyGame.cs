807e5066a034abe5866f50f593a372abe4ce8927
Yukki16 <89789213+Yukki16@users.noreply.github.com>
2022-02-08 11:26:09 +0100

    .

--- GXPEngine2022BB/GXPEngine/MyGame.cs
+++ GXPEngine2022BB/GXPEngine/MyGame.cs
@@ -1,12 +1,15 @@
 using System;									// System contains a lot of default C# libraries 
 using GXPEngine;                                // GXPEngine contains the engine
-using System.Drawing;							// System.Drawing contains drawing tools such as Color definitions
+using System.Drawing;                           // System.Drawing contains drawing tools such as Color definitions
+using GXPEngine.GXPEngine;
 
 public class MyGame : Game
 {
+	Combo comboFirstPlayer;
 	public MyGame() : base(800, 600, false)		// Create a window that's 800x600 and NOT fullscreen
 	{
-		// Draw some things on a canvas:
+
+		/*// Draw some things on a canvas:
 		EasyDraw canvas = new EasyDraw(800, 600);
 		canvas.Clear(Color.MediumPurple);
 		canvas.Fill(Color.Yellow);
@@ -18,7 +21,9 @@ public class MyGame : Game
 
 		// Add the canvas to the engine to display it:
 		AddChild(canvas);
-		Console.WriteLine("MyGame initialized");
+		Console.WriteLine("MyGame initialized");*/
+		comboFirstPlayer = new Combo();
+		AddChild(comboFirstPlayer);
 	}
 
 	// For every game object, Update is called every frame, by the engine:

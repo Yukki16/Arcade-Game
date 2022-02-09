8b037ba2176a22eb2e7ac32ca6a6be5006ad6a41
vostrenkoff <iseyth@gmail.com>
2022-02-09 11:31:06 +0100

    settings

--- GXPEngine2022BB/GXPEngine/MyGame.cs
+++ GXPEngine2022BB/GXPEngine/MyGame.cs
@@ -9,14 +9,14 @@ public class MyGame : Game
 
 	PlayerKeysCombo playerOne;
 	PlayerKeysCombo playerTwo;
-	public MyGame() : base(800, 600, false)
+	public MyGame() : base(Settings.Width, Settings.Height, Settings.FullScreen, true, Settings.ScreenResolutionX, Settings.ScreenResolutionY)
 	{
 		playerOne = new PlayerKeysCombo(ManagerAndStuff.Player.P1);
-		playerOne.SetKeys(65, 87, 68);
+		playerOne.SetKeys(Settings.P1Left, Settings.P1Up, Settings.P1Right);
+		AddChild(playerOne);
 
 		playerTwo = new PlayerKeysCombo(ManagerAndStuff.Player.P2);
-		playerTwo.SetKeys(285, 283, 286);
-		AddChild(playerOne);
+		playerTwo.SetKeys(Settings.P2Left, Settings.P2Up, Settings.P2Right);
 		AddChild(playerTwo);
 
 	}
@@ -25,12 +25,25 @@ public class MyGame : Game
 	void Update()
 	{
 		
-		
-		
 	}
 
-	static void Main()							// Main() is the first method that's called when the program is run
+	static void Main()
 	{
-		new MyGame().Start();					// Create a "MyGame" and start it
+		Settings.Load();
+		if (Settings.FullScreen)
+		{
+			try
+			{
+				new MyGame().Start();
+			}
+			catch (Exception error)
+			{
+				Console.WriteLine("Error: {0}\n terminating game.", error.Message);
+			}
+		}
+		else
+		{
+			new MyGame().Start();
+		}
 	}
 }
